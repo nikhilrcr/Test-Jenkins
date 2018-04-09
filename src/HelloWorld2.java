c29747d43225f0b6649d82a65999da714f0e1dce
nikhilrcr <nikhilrcr@gmail.com>
2018-04-09 20:53:25 +0530

    This is commit to test jenkins build step

--- src/HelloWorld2.java
+++ src/HelloWorld2.java
@@ -4,6 +4,7 @@ public class HelloWorld2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
+		System.out.println("This is to verify jeniks build step");
 
 	}
 
